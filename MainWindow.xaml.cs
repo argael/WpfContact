0a43c9b6514d0e0b51199020be11ffbc5416faff
Argael <argael@passtech.be>
2023-01-19 14:14:33 +0100

    Use contact class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfContact.Models;
 
 namespace WpfContact
 {
@@ -27,32 +28,46 @@ namespace WpfContact
 
 		private void OnClickBtnContactAdd(object sender, RoutedEventArgs e)
 		{
-			if (string.IsNullOrEmpty(txtContactName.Text))
+			if (!ValidateForm())
+				return;
+
+			Contact contact = new(txtContactName.Text, txtContactPhone.Text);
+			if (!contact.isValid())
 			{
-				MessageBox.Show("Please enter a name for the contact");
+				MessageBox.Show("This contact is invalid");
 				return;
 			}
-				
 
-			if (string.IsNullOrEmpty(txtContactPhone.Text))
-			{
-				MessageBox.Show("Please enter a phone number for the contact");
-				return;
-			}
-				
-
-			string contact = $"{txtContactName.Text.Trim()} ({txtContactPhone.Text.Trim()})";
-
-			if (lstNames.Items.Contains(contact))
+			if (lstNames.Items.Contains(contact.ToString()))
 			{
 				MessageBox.Show("This contact already exist");
 				return;
 			}
 
-			lstNames.Items.Add(contact);
+			lstNames.Items.Add(contact.ToString());
 
 			txtContactName.Clear();
 			txtContactPhone.Clear();
+		}
+
+		/// <summary>
+		/// Test if the posted field are valid.
+		/// </summary>
+		private bool ValidateForm()
+		{
+			if (string.IsNullOrEmpty(txtContactName.Text))
+			{
+				MessageBox.Show("Please enter a name for the contact");
+				return false;
+			}
+
+			if (string.IsNullOrEmpty(txtContactPhone.Text))
+			{
+				MessageBox.Show("Please enter a phone number for the contact");
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
---
+++ Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfContact.Models
+{
+	public class Contact
+	{
+		public string? Name { get; set; }
+
+		public string? Phone { get; set; }
+
+		public Contact(string? name, string? phone)
+		{
+			Name = name?.Trim(); 
+			Phone = phone?.Trim();
+		}
+
+		public bool isValid()
+		{
+			return !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Phone);
+		}
+
+		public override string ToString()
+		{
+			return $"{Name} ({Phone ?? "n.a."})".Trim();
+		}
+	}
+}
